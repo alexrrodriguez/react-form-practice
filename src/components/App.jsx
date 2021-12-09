9996724d96b6b2e6cd66587d3f2df0d780f4dce0
alex <arodriguez202020@gmail.com>
2021-12-09 16:43:51 -0600

    create onChange event to input and handleChange function that passes event to store input value and display placeholder and type values

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 function App() {
+  function handleChange(event) {
+    console.log(event.target.value);
+    console.log(event.target.placeholder);
+    console.log(event.target.type);
+  }
+
   return (
     <div className="container">
       <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
+      <input onChange={handleChange} type="text" placeholder="What's your name?" />
       <button>Submit</button>
     </div>
   );
