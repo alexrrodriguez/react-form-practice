4b350147a7f8af16d7f717c99678818f9d7d073f
alex <arodriguez202020@gmail.com>
2021-12-09 16:50:55 -0600

    add name object to h1 and use useState to set name variable to empty string,  add setName function to handleChange and pass in value of handleChange event

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
   function handleChange(event) {
     console.log(event.target.value);
-    console.log(event.target.placeholder);
-    console.log(event.target.type);
+    setName(event.target.value);
   }
 
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input onChange={handleChange} type="text" placeholder="What's your name?" />
+      <h1>Hello {name}</h1>
+      <input onChange={handleChange} type="text" placeholder="What's your name?" value={name} />
       <button>Submit</button>
     </div>
   );
